8310612e28c1566e2e228c9e0843acc371f7d64d
Viktor Ielnykov <yelnikoff@gmail.com>
2024-01-10 11:15:20 +0100

    Initial commit. Add generated java application

---
+++ src/main/java/eu/yelnikoff/curverter/CurverterApplication.java
@@ -0,0 +1,13 @@
+package eu.yelnikoff.curverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/yelnikoff/curverter/CurverterApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.yelnikoff.curverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
